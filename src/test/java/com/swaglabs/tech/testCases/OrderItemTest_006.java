658b08db5788f2dda18f5edaa6857da79e8e8668
Alexey Solovyev <solovevalex@gmail.com>
2021-07-13 21:53:00 -0400

    Order Item for problem_user has been created, tested and added to the project

---
+++ src/test/java/com/swaglabs/tech/testCases/OrderItemTest_006.java
@@ -0,0 +1,44 @@
+package com.swaglabs.tech.testCases;
+
+import com.swaglabs.tech.pageObjects.OrderItemPage;
+import org.testng.annotations.Test;
+
+// Order and item with problem_user
+
+public class OrderItemTest_006 extends BaseClass {
+
+    @Test
+    public void orderItemTest() throws Exception {
+        driver.get(baseURL);
+        OrderItemPage orderPage = new OrderItemPage(driver);
+
+        orderPage.checkLogo();
+        orderPage.setUserName(problemUser);
+        orderPage.setPassword(password);
+        orderPage.clickLoginButton();
+        orderPage.checkProductsPage();
+        orderPage.openItemCard();
+        orderPage.checkItemCard();
+        orderPage.clickAddToCartButton();
+        orderPage.checkCartIcon();
+        orderPage.clickBackToProductsButton();
+        orderPage.checkProductsPage();
+        orderPage.clickOnCartIcon();
+        orderPage.checkCartPage();
+        orderPage.clickCheckoutButton();
+        orderPage.setFirstUserName(firstUserName);
+        orderPage.setLastUserName(lastUserName);
+        orderPage.setZipCode(zipCode);
+        orderPage.clickContinueButton();
+        orderPage.checkCheckoutTitle();
+        orderPage.checkTotal();
+        orderPage.clickFinishButton();
+        orderPage.checkCompleteOrderMessage();
+        orderPage.clickBackHomeButton();
+        orderPage.checkProductsPage();
+        orderPage.clickMenuButton();
+        orderPage.clickLogout();
+        orderPage.checkLoginButton();
+    }
+
+}
